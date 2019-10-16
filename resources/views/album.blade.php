9828974e31c8913014a6102e3a60fc0384498743
madhusamnka2016 <smmailofficial@gmail.com>
2019-10-16 19:58:39 +0530

    Album generation Finized

--- resources/views/album.blade.php
+++ resources/views/album.blade.php
@@ -4,101 +4,71 @@
 @section('content')
 	  <!-- ***** Hero Area Start ***** -->
 		<!-- start banner Area -->
-		<section class="banner-area relative about-banner" id="home">	
+		@if(sizeof($album)>0)                                                              
+			@foreach($album as $item)
+		<section class="banner-area relative about-banner" style="background:url({{$item->a_thumb}}" id="home">	
 			<div class="overlay overlay-bg"></div>
 			<div class="container">				
 				<div class="row d-flex align-items-center justify-content-center">
+				
 					<div class="about-content col-lg-12">
 						<h1 class="text-white">
-							Gallery	Page			
+								{{$item->a_title}}			
 						</h1>	
 						<h3 class="text-white">
-							Gallery	Page			
+								{{$item->a_desc}}			
 						</h3>
 						<h5 class="text-white">
-							Gallery	Page			
+								{{$item->a_date}}			
 						</h5>
-						<p class="text-white link-nav"><a href="index.html">Home </a>  <span class="lnr lnr-arrow-right"></span>  <a href="gallery.html"> Gallery</a></p>
+						<p class="text-white link-nav"><a href="{{ url('/') }}">Home </a>  <span class="lnr lnr-arrow-right"></span>  <a href="{{ url('/Gallery') }}"> Gallery</a></p>
 					</div>	
 				</div>
+				
 			</div>
 		</section>
+		@endforeach
+		@else                                
+		@endif 
 		<!-- End banner Area -->	
 			
 		<!-- Start gallery Area -->
 		<section class="gallery-area section-gap">
 			<div class="container">
 				<div class="row">
-					<div class="col-lg-7">
-						<a href="{{asset ('img/gallery/g1.jpg')}}" class="img-gal">
-							<div class="single-imgs relative">		
-								<div class="overlay overlay-bg"></div>
-								<div class="relative">				
-									<img class="img-fluid" src="{{asset ('img/gallery/g1.jpg')}}" alt="">		
-								</div>
+					@if(sizeof($album)>0)                                                              
+					@foreach($album as $item)
+					
+					@for ($i = 0; $i < ((int)$item->a_count); $i++)
+
+					
+						<div class="col-lg-3 pb-30">
+							<div class="single-carusel row align-items-center">
+								<div class="col-12 col-md-12 thumb">
+										<a href="{{$item->a_source}}{{$i+1}}.jpg" class="img-gal"><img class="img-fluid" src="{{$item->a_source}}{{$i+1}}.jpg" alt="">
+										</a>
+										</div>
+								
 							</div>
-						</a>
-					</div>
-					<div class="col-lg-5">
-						<a href="{{asset ('img/gallery/g2.jpg')}}" class="img-gal">
-							<div class="single-imgs relative">		
-								<div class="overlay overlay-bg"></div>
-								<div class="relative">				
-									<img class="img-fluid" src="{{asset ('img/gallery/g2.jpg')}}" alt="">				
-								</div>
-							</div>
-						</a>
-					</div>
-					<div class="col-lg-4">
-						<a href="{{asset ('img/gallery/g3.jpg')}}" class="img-gal">
-							<div class="single-imgs relative">		
-								<div class="overlay overlay-bg"></div>
-								<div class="relative">				
-									<img class="img-fluid" src="{{asset ('img/gallery/g3.jpg')}}" alt="">				
-								</div>
-							</div>
-						</a>
-					</div>
-					<div class="col-lg-4">
-						<a href="{{asset ('img/gallery/g4.jpg')}}" class="img-gal">
-							<div class="single-imgs relative">		
-								<div class="overlay overlay-bg"></div>
-								<div class="relative">					
-									<img class="img-fluid" src="{{asset ('img/gallery/g4.jpg')}}" alt="">				
-								</div>
-							</div>
-						</a>
-					</div>
-					<div class="col-lg-4">
-						<a href="{{asset ('img/gallery/g5.jpg')}}"  class="img-gal">
-							<div class="single-imgs relative">		
-								<div class="overlay overlay-bg"></div>
-								<div class="relative">					
-									<img class="img-fluid" src="{{asset ('img/gallery/g5.jpg')}}" alt="">				
-								</div>
-							</div>
-						</a>
-					</div>
-					<div class="col-lg-5">
-						<a href="{{asset ('img/gallery/g6.jpg')}}" class="img-gal">
-							<div class="single-imgs relative">		
-								<div class="overlay overlay-bg"></div>
-								<div class="relative">				
-									<img class="img-fluid" src="{{asset ('img/gallery/g6.jpg')}}" alt="">				
-								</div>
-							</div>
-						 </a>
-					</div>
-					<div class="col-lg-7">
-						<a href="{{asset ('img/gallery/g7.jpg')}}" class="img-gal">
-							<div class="single-imgs relative">		
-								<div class="overlay overlay-bg"></div>
-								<div  class="relative">					
-									<img class="img-fluid" src="{{asset ('img/gallery/g7.jpg')}}" alt="">				
-								</div>
-							</div>
-						</a>
-					</div>
+						</div>
+					
+							{{-- <div class="col-lg-7">
+								<a href="{{$item->a_source}}{{$i+1}}.jpg" class="img-gal">
+									<div class="single-imgs relative">		
+										<div class="overlay overlay-bg"></div>
+										<div class="relative">				
+											<img class="img-fluid" style="width:25%; hight:25%" src="{{$item->a_source}}{{$i+1}}.jpg" alt="">		
+										</div>
+									</div>
+								</a>
+							</div> --}}
+						@endfor
+					@endforeach
+					                               
+					@endif 
+					
+					
+					
 				</div>
 			</div>	
 		</section>
